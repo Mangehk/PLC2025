2f4141b6ae427ed135b420978a09ca72279e5790
Mangeh Kulkarni <mangeshkulkarni2801@gmail.com>
2025-04-29 10:57:31 +0000

    Modified code to work correctly on gitpod

--- lab2-functionalprimer/haskell/task4.hs
+++ lab2-functionalprimer/haskell/task4.hs
@@ -1,14 +1,16 @@
-ask :: String -> IO ()
-ask prompt =
-  do
-  putStrLn prompt
+ask :: String -> Int -> IO ()
+ask prompt count = do
+  putStrLn (prompt ++ replicate count '!')
   line <- getLine
   if line == ""
-    then ask prompt
-    else putStrLn ("you said: " ++ reverse line)
+    then ask prompt (count + 1)
+    else if line == "quit"
+      then putStrLn "quitting..."
+      else do 
+        putStrLn ("you said: " ++ reverse line)
+        ask prompt 0
 
 main :: IO ()
-main =
-  do
-  let prompt = "please say something"
-  ask prompt
+main = do
+  let prompt = "please say something (type 'quit' to exit)"
+  ask prompt 0
