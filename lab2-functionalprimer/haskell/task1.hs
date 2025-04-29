3ef1e8892eeb528ced5edaf82f80bb381dc1ca1d
Mangeh Kulkarni <mangeshkulkarni2801@gmail.com>
2025-04-29 10:48:40 +0000

    Modified code to work correctly on gitpod

--- lab2-functionalprimer/haskell/task1.hs
+++ lab2-functionalprimer/haskell/task1.hs
@@ -1,11 +1,16 @@
 module Main where
 
-main =
-    do
+main :: IO ()
+main = do
     print (myFunc1 5)
-    --print (MyFunc2 10)
-    --print (3rdFunc)
+    print (myFunc2 10)
+    print thirdFunc
 
-myFunc1 x = x*10
---MyFunc2 x = x*2
---3rdFunc = "Hello, this is 3rd Func"
+myFunc1 :: Int -> Int
+myFunc1 x = x * 10
+
+myFunc2 :: Int -> Int
+myFunc2 x = x * 2
+
+thirdFunc :: String
+thirdFunc = "Hello, this is thirdFunc"
